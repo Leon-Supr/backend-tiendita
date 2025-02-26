f44b32431f2abc62420aab211a761f068e91d7c4
Leonardo Arguello <Leon-Supr@users.noreply.github.com>
2025-02-26 14:34:41 -0600

    Add initial Express server setup with MVC structure and models directory

---
+++ index.js
@@ -0,0 +1,21 @@
+// Modelo vista controlador
+
+//#1 Importar express
+const express = require('express')
+const productRoutes = require('./routes/productRoutes')
+
+const PORT = process.env.PORT || 3000
+
+//#2 Crear aplicación/instancia de express
+const app = express()
+
+//#3 Configurar express para que entienda JSON
+app.use(express.json())
+
+//#4 Crear las rutas de la aplicación
+//app.use('/api/v1', productRoutes)
+
+//#5 Levantar el servidor
+app.listen(PORT, ()=>{
+    console.log('Server is running mi loco');
+})
---
+++ models/Products.js
@@ -0,0 +1,6 @@
+// MODELO - Interactua con la base de datos
+// NO VALIDA DATOS O RESOLVER PROMESAS (eso es del controlador)
+
+const knex = require('../config')
+
+// De config exportamos Knex con la variable de entorno
