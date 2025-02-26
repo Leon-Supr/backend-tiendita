e954625f0438c078276aaf014210b0556ff2d85d
Leonardo Arguello <Leon-Supr@users.noreply.github.com>
2025-02-25 18:20:17 -0600

    Add sales migration

---
+++ migrations/20250226000154_sales.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.hasTable('sales').then(function (exists) {
+        if (!exists) {
+            return knex.schema.createTable('sales', function (table) {
+                table.increments('sale_id').primary().unique()
+                table.timestamp('created_at').defaultTo(knex.fn.now())
+                table.integer('fk_customer').unsigned().references('customers.customer_id')
+                table.boolean('active').notNullable().defaultTo(true)
+            })
+        }
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.hasTable('sales').then(function (exists) {
+        if (exists) {
+            return knex.schema.dropTable('sales')
+        }
+    })
+};
